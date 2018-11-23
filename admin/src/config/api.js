99694b8a8f15e310f5c0d55638ea28dea83db4d8
BengBu-YueZhang <zhangyue_coder@163.com>
2018-11-23 13:45:27 +0800

    update

---
+++ admin/src/config/api.js
@@ -0,0 +1,5 @@
+const API = {
+  bing: 'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1'
+}
+
+export default API
---
+++ admin/src/routes/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class LoginPage extends React.Component {
+  render () {
+    return (
+      <section></section>
+    )
+  }
+}
+
+export default LoginPage
