c09f74fedf2c7934e10832b9afd532390e69d0d3
BengBu-YueZhang <zhangyue_coder@163.com>
2018-11-23 16:58:40 +0800

    update

--- admin/src/routes/Login.js
+++ admin/src/routes/Login.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Form, Icon, Input, Button, Checkbox } from 'antd'
+import { Form, Icon, Input, Button } from 'antd'
+import styles from './Login.css'
 
 class LoginPage extends React.Component {
 
@@ -11,25 +12,28 @@ class LoginPage extends React.Component {
 
   render () {
     return (
-      <section>
-        <Form onSubmit={this.handleSubmit} className="login-form">
-          <Form.Item>
-            <Input
-              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              placeholder="用户名"
-            />
-          </Form.Item>
-          <Form.Item>
-            <Input
-              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              type="password"
-              placeholder="密码"
-            />
-          </Form.Item>
-          <Button type="primary" htmlType="submit" className="login-form-button">
-            登录
-          </Button>
-        </Form>
+      <section className={styles.root}>
+        <div className={styles.formWrapper}>
+          <h3 className={styles.title}>血压体重管理系统</h3>
+          <Form onSubmit={this.handleSubmit} className="login-form">
+            <Form.Item>
+              <Input
+                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                placeholder="用户名"
+              />
+            </Form.Item>
+            <Form.Item>
+              <Input
+                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                type="password"
+                placeholder="密码"
+              />
+            </Form.Item>
+            <Button block type="primary" htmlType="submit" className="login-form-button">
+              登录
+            </Button>
+          </Form>
+        </div>
       </section>
     )
   }
