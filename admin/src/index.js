afe3063f97a534d46b9adb04764dadc2d2bd3c5b
BengBu-YueZhang <zhangyue_coder@163.com>
2018-11-23 16:37:32 +0800

    update

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,4 +1,5 @@
 import dva from 'dva';
+import 'antd/dist/antd.css'
 
 // 1. Initialize
 const app = dva();
--- admin/src/routes/Login.js
+++ admin/src/routes/Login.js
@@ -1,14 +1,35 @@
 import React from 'react'
+import { Form, Icon, Input, Button, Checkbox } from 'antd'
 
 class LoginPage extends React.Component {
 
   componentDidMount () {
   }
 
+  handleSubmit = () => {
+  }
+
   render () {
     return (
       <section>
-        登录页
+        <Form onSubmit={this.handleSubmit} className="login-form">
+          <Form.Item>
+            <Input
+              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder="用户名"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Input
+              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              type="password"
+              placeholder="密码"
+            />
+          </Form.Item>
+          <Button type="primary" htmlType="submit" className="login-form-button">
+            登录
+          </Button>
+        </Form>
       </section>
     )
   }
